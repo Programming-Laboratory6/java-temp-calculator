1b4edfeca51cefdf4ea94e493a36d65f12ae7176
andreluiz93 <brasilandre93@hotmail.com>
2015-09-22 19:36:28 -0300

    Improvement implementation

--- src/main/java/br/edu/univas/si/lab6/calculator/service/TempService.java
+++ src/main/java/br/edu/univas/si/lab6/calculator/service/TempService.java
@@ -3,9 +3,7 @@ package br.edu.univas.si.lab6.calculator.service;
 public class TempService {
 	
 	public static Float getIndex(Float celsius){
-		Float c = (float) 32;
-		Float c2 = (float) 1.8;
-		return celsius * c2 + c;
+		return (float) (celsius * 32 + 1.8);
 	}
 
 }
